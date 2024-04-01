d359ff197555cef31cb30c1b866447b3371b9af1
LucasTKP <lucasgeansantos@gmail.com>
2024-04-01 10:05:18 -0300

    change title in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    WidgetsFlutterBinding.ensureInitialized();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Currency converter',
+      title: 'Cambio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
